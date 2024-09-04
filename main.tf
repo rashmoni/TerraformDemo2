98af1e9e5c46808dab29b7f3ce4d2369412f9894
rashmoni <rashudey9851@gmail.com>
2024-09-04 14:18:56 +0200

    Create V net and subnet

--- main.tf
+++ main.tf
@@ -1,42 +1,34 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-
-resource "random_pet" "rg_name" {
-  prefix = "rg"
-}
-
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  location = "swedencentral"
-  name     = "MYRG11"
+  location = var.resource_group_location
+  name     = "${random_pet.prefix.id}-rg"
 }
 
-#vnet
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet1"
+# Virtual Network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${random_pet.prefix.id}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "swedencentral"
-  resource_group_name = "MYRG10"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Subnet 1
 resource "azurerm_subnet" "my_terraform_subnet_1" {
-  name                 = "subnet1"
-  resource_group_name  = "MYRG10"
-  virtual_network_name = "vnet1"
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.0.0/24"]
+}
+
+# Subnet 2
+resource "azurerm_subnet" "my_terraform_subnet_2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "random_pet" "prefix" {
+  prefix = var.resource_group_name_prefix
+  length = 1
 }
