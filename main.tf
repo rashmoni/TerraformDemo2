b8886946e18047ca9f860ba153fdd9b355eefadb
rashmoni <rashudey9851@gmail.com>
2024-09-04 13:19:32 +0200

    added vnet and subnet

--- main.tf
+++ main.tf
@@ -24,3 +24,19 @@ resource "azurerm_resource_group" "rg" {
   location = "swedencentral"
   name     = "MYRG10"
 }
+
+#vnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet1"
+  address_space       = ["10.0.0.0/16"]
+  location            = "swedencentral"
+  resource_group_name = "MYRG10"
+}
+
+# Subnet 1
+resource "azurerm_subnet" "my_terraform_subnet_1" {
+  name                 = "subnet-1"
+  resource_group_name  = "MYRG10"
+  virtual_network_name = "vnet1"
+  address_prefixes     = ["10.0.0.0/24"]
+}
