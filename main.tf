fc8a19148c6cb857b862e68c3a6719e18bb51bec
rashmoni <rashudey9851@gmail.com>
2024-09-04 11:40:14 +0200

    First Commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "random_pet" "rg_name" {
+  prefix = "rg"
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "swedencentral"
+  name     = "MYRG2"
+}
